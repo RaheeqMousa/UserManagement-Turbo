adee4862e46868c052f0a2522d1c7647f7542d01
RaheeqMousa <raheeqmousa00@gmail.com>
2026-08-23 11:31:27 +0300

    first commit

---
+++ src/test/java/com/example/TurboUserManagament/TurboUserManagamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TurboUserManagament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurboUserManagamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
